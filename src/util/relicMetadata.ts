6ff85f9f7a07320a2980d5a0dc36e965aa9a0a50
Adam Davies <acdavies001@gmail.com>
2021-08-10 17:17:49 -0500

    Add relic images and IDs

--- src/util/relicMetadata.ts
+++ src/util/relicMetadata.ts
@@ -1,60 +1,74 @@
 export default [
   [
     {
-      name: 'Man',
-      image: 'https://i.imgur.com/PPJaqw6.png',
+      id: 1,
+      name: 'Unarmed Man',
+      image: 'https://i.imgur.com/pis8YBK.png',
     },
     {
+      id: 2,
       name: 'Man with Club',
-      image: '',
+      image: 'https://i.imgur.com/oIA9BAz.png',
     },
     {
+      id: 3,
       name: 'Man with Club and Shield',
-      image: '',
+      image: 'https://i.imgur.com/hBO9Ghy.png',
     },
     {
-      name: 'Blowdart',
-      image: '',
+      id: 4,
+      name: 'Man with Blowpipe',
+      image: 'https://i.imgur.com/6I1ZJTp.png',
     },
     {
+      id: 5,
       name: 'Pharaoh',
-      image: '',
+      image: 'https://i.imgur.com/gUDWRIo.png',
     },
     {
+      id: 6,
       name: 'UFO',
-      image: '',
+      image: 'https://i.imgur.com/kHnuU7i.png',
     },
     {
+      id: 7,
       name: 'Bowl',
-      image: '',
+      image: 'https://i.imgur.com/qMl0pOb.png',
     },
     {
+      id: 8,
       name: 'Angry Demon',
-      image: '',
+      image: 'https://i.imgur.com/2vESR9d.png',
     },
     {
+      id: 9,
       name: 'Pyramid Head',
-      image: '',
+      image: 'https://i.imgur.com/zOKGwzL.png',
     },
     {
+      id: 10,
       name: 'Sphere',
-      image: '',
+      image: 'https://i.imgur.com/GgAlfm2.png',
     },
     {
+      id: 11,
       name: 'Snake',
-      image: '',
+      image: 'https://i.imgur.com/k7xoZT7.png',
     },
     {
+      id: 12,
       name: 'Staring Man',
-      image: '',
+      image: 'https://i.imgur.com/SFyEy5h.png',
     },
     {
+      id: 13,
       name: 'Skull',
-      image: '',
+      image: 'https://i.imgur.com/bTnE4iV.png',
     },
     {
+      id: 14,
       name: 'Dragon Head',
-      image: '',
+      image: 'https://i.imgur.com/jMlAOgK.png',
     },
   ],
   [
